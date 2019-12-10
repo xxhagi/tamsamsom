c0189290e0dbd0c95732c4e782d635d4063bff52
xxhagi <58281883+xxhagi@users.noreply.github.com>
2019-12-10 21:48:00 +0900

    Add files via upload

--- routes/qna.js
+++ routes/qna.js
@@ -2,11 +2,121 @@ module.exports = function(app, conn, upload) {
     var express = require('express');
     var router = express.Router();
     
-    router.get('/', function(req, res, next) {
-      res.render('qna/qna', {
-        title: "Hanguler"
-      });
-    });
+
+      router.get('/', (req, res) => {
+        var sql = 'SELECT * FROM qna';
+        conn.query(sql, [], function(err, qna, fields){
+          if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error: ' + err);
+          }
+          else{
+            res.render('qna/list', {
+              'qna': qna
+            })
+          }
+        })
+      })
     
-    return router;
-    };
+      router.get('/add', (req, res) => {
+        res.render('qna/add')
+      })
+      router.post('/add', (req, res) => {
+        var title = req.body.title;
+        var text = req.body.text;
+        var sql = 'INSERT INTO qna (`title`, `text`, `inserted`, `updated`) VALUES(?, ?, now(), now())';
+        conn.query(sql, [title, text], function(err, result, fields){
+          if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error: ' + err)
+          }
+          else{
+            res.redirect('/qna/' + result.insertId);
+          }
+        })
+      })
+      router.get('/:id', (req, res) => {
+        var id = req.params.id;
+        var sql = 'SELECT * FROM qna WHERE id=?';
+        conn.query(sql, [id], function(err, qnas, fields){
+          if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error: ' + err);
+          }
+          else{
+            res.render('qna/detail', {
+              id:id,
+              qna:qnas[0]
+            })
+          }
+        })
+      })
+    
+      router.get('/:id/edit', (req, res) => {
+        var id = req.params.id;
+        var sql = 'SELECT * FROM qna WHERE id=?';
+        conn.query(sql, [id], function(err, qnas, fields){
+          if(err){
+              console.log(err);
+              res.status(500).send('Internal Server Error: ' + err);
+          }
+          else{
+            res.render('qna/edit', {
+              qna: qnas[0]
+            })
+          }
+        })
+      })
+    
+      /* 수정 */
+      router.post('/:id/edit', (req, res) => {
+        var id = req.params.id;
+        var title = req.body.title;
+        var text = req.body.text;
+        var sql = 'UPDATE qna SET `title` = ?, `text` = ?, `updated` = now() WHERE id=?';
+        conn.query(sql, [title, text, id], function(err, result, fields){
+          if(err){
+            console.log(err);
+            res.status(500).send('Internal Server Error: ' + err);
+          }
+          else{
+            res.redirect('/qna/' + id)
+          }
+        })
+      })
+    
+      
+  /* Delete confirmation */
+  router.get('/:id/delete', (req, res) => {
+    var id = req.params.id;
+    var sql = 'SELECT * FROM qna WHERE id=?';
+    conn.query(sql, [id], function(err, news, fields){
+      if(err){
+        console.log(err);
+        res.status(500).send('Internal Server Error: ' + err);
+      } else {
+        res.render('qna/delete', {news:news[0]});
+      }
+    });
+  });
+
+  /* DELETE DB row */
+  router.post('/:id/delete', (req, res) => {
+    var id = req.params.id;
+
+    var sql = 'DELETE FROM qna WHERE id = ?';
+    conn.query(sql, [id], function(err, result, fields){
+      if(err){
+        console.log(err);
+        res.status(500).send('Internal Server Error: ' + err);
+      } else {
+        res.redirect('/qna/');
+      }
+    });
+  });
+    
+      
+
+      return router
+    }
+    
